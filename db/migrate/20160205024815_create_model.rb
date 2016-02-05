26171b86e54ec2d1e1798a757a85229ca098190d
William Bruntrager <w.bruntrager@gmail.com>
2016-02-04 20:51:15 -0600

    edited migration

--- db/migrate/20160205024815_create_model.rb
+++ db/migrate/20160205024815_create_model.rb
@@ -1,4 +1,11 @@
 class CreateModel < ActiveRecord::Migration
-  def change
+  def up
+  	create_table :models do |t|
+  		t.string :name
+  	end
+  end
+
+  def down
+  	drop_table :models
   end
 end
